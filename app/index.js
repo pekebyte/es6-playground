8285ca67ca1ad2a6d331f74aa1dde79ca2358682
Pedro Molina <pedrom@pekebyte.com>
2017-11-05 20:51:58 -0400

    Closures

--- app/index.js
+++ app/index.js
@@ -147,3 +147,112 @@ let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
 
 console.log(harry.greet());
 console.log(harry.info());
+
+// Data Structures - Set
+let st1 = new Set();
+st1.add(5);
+st1.add({x:1,y:2});
+st1.add("Just testing sets")
+console.log(st1);
+console.log(st1.size);
+console.log(st1.has(5));
+
+let nmrs1 = [4,6,8,10,12,14];
+let st2 = new Set(nmrs1);
+
+for(let element of st2.values()){
+	console.log(element);
+}
+
+let chrs = 'asfdsfokiodsepwro[xc,xz.cv,ds;lfkqas;dfgkasfgassd';
+let chrsArr = chrs.split('');
+let chrsSt= new Set(chrsArr);
+
+
+//Data Structures - Map
+let mp1 = new Map();
+
+mp1.set("key 1","return value for a string key");
+mp1.set({a: 'key'}, "return value for a object key");
+mp1.set(()=>{},'return value for a function key');
+console.log(mp1);
+
+let nmrs2 = [[1,"one"],[2,"two"],[3,"three"],[4,"four"],[5,"five"]];
+
+let mp2 = new Map(nmrs2);
+
+for (let [key, value] of mp2.entries()){
+	console.log(`${key} points to ${value}`);
+}
+
+//Exercise
+let string = 'supercalifragilisticexpialidocious';
+
+const countLetter = (word, orig_letter) => {
+  let letters = new Map();
+  for (let i=0; i<word.length; i++) {
+    let letter = word[i];
+    if (!letters.has(letter)) {
+      letters.set(letter,1);
+    } else {
+      letters.set(letter, letters.get(letter) + 1);
+    }
+  }
+  return letters.get(orig_letter);
+};
+
+let a_count = countLetter(string, 'a');
+let x_count = countLetter(string, 'x');
+console.log(a_count);
+
+//Closures
+let callReveal = () => {
+	let secret = 'ES6 Rocks!';
+
+	let reveal = () => {
+		console.log(secret);
+	}
+
+	reveal();
+}
+
+callReveal();
+
+//Function Factories
+const addSuffix = (x) => {
+	const concat = (y) => {
+		return y + x;
+	}
+	return concat;
+}
+
+let addNess = addSuffix("ness");
+let addHappi = addNess("happi");
+
+console.log(addHappi);
+
+const mathMultiply = x => y => y*x;
+
+let nojom = mathMultiply(2);
+let mplyRslt = nojom(9);
+console.log(mplyRslt);
+
+//Private methods using closures
+const budget = () => {
+	let balance = 0;
+	let changeBal = (val) => {
+		return balance += val;
+	}
+	const deposit20 = () => changeBal(20);
+	const check = () => balance;
+
+	return {
+		deposit20: deposit20,
+		check: check
+	}
+}
+
+let wallet = budget();
+wallet.deposit20();
+
+console.log(wallet.check());
